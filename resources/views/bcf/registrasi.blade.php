d883d893d95cb5ea7b0c54f157c78fa844c3ab7f
RP-Group-Code <mhasurungan54@gmail.com>
2026-08-09 18:08:36 +0700

    udpate bcf

--- resources/views/bcf/registrasi.blade.php
+++ resources/views/bcf/registrasi.blade.php
@@ -201,9 +201,10 @@
             <div class="bcf-brand">BRI <span>Branch Office Palembang Sriwijaya</span></div>
             <div class="bcf-hero-inner">
                 <div class="bcf-hero-copy">
-                    <span class="bcf-kicker">“The Golden Dynasty of Sriwijaya: Rise of the Royals”</span>
+                    <span class="bcf-kicker pt-4">“BCF SRIWIJAYA 2026”</span>
                     <img class="bcf-hero-logo" src="{{ asset('images/bcf-logo2.png') }}" alt="BRILiaN Culture Fest 2026">
-                    <p>Registrasi Kepersertaan Acara BCF Branch Office Sriwijaya. Silakan pilih menu yang ingin Anda gunakan.</p>
+                    <span class="bcf-kicker pt-4">“BCF SRIWIJAYA 2026”</span>
+                    
                     <div class="bcf-hero-actions">
                         <a class="bcf-btn bcf-btn-primary" href="#registrasi"><i class="fa-solid fa-pen-to-square me-2"></i>REGISTRASI</a>
                         <a class="bcf-btn bcf-btn-secondary" href="#peserta"><i class="fa-solid fa-users me-2"></i>CEK DATA PESERTA</a>
